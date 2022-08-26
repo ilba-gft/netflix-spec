380dce7d98f747d04a84660cf754f7171b321559
Judith <judith.barrajon@gmail.com>
2022-08-26 14:30:53 +0200

    Commit final

--- netflix-group2/src/main/java/com/netflix/group2/controllers/RecommendController.java
+++ netflix-group2/src/main/java/com/netflix/group2/controllers/RecommendController.java
@@ -1,4 +1,20 @@
 package com.netflix.group2.controllers;
 
+import com.netflix.group2.entities.Title;
+import com.netflix.group2.repositories.TitleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+@RestController
 public class RecommendController {
+    @Autowired
+    TitleRepository titleRepository;
+
 }
