4b4d7161f3db076d4d8b92c79420881847449d61
ilba-gft <ilba@gft.com>
2022-08-26 10:12:09 +0200

    Initial commit

---
+++ netflix-group2/src/main/java/com/netflix/group2/NetflixGroup2Application.java
@@ -0,0 +1,13 @@
+package com.netflix.group2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixGroup2Application.class, args);
+	}
+
+}
---
+++ netflix-group2/src/test/java/com/netflix/group2/NetflixGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.group2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixGroup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
